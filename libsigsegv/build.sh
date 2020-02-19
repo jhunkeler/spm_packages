5bbdc51df0dea2b2238036f6af51220b411f097e
Joseph Hunkeler <jhunkeler@gmail.com>
2020-02-19 09:57:56 -0500

    Initial commit of libsigsegv

---
+++ libsigsegv/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+name=libsigsegv
+version=2.12
+revision=0
+sources=(
+    "https://ftp.gnu.org/gnu/${name}/${name}-${version}.tar.gz"
+)
+build_depends=()
+depends=()
+
+function prepare() {
+    tar xf ${name}-${version}.tar.gz
+    cd ${name}-${version}
+}
+
+function build() {
+    ./configure --prefix=${_prefix}
+    make -j${_maxjobs}
+}
+
+function package() {
+    make install DESTDIR="${_pkgdir}"
+}
